3f2ff17db08736a92a42f1cc69ed8d58a5b12fbf
Alfardil Alam <fardilfayed@gmail.com>
2025-05-27 21:53:43 -0400

    added a script to easily open up my java practice folder

---
+++ javaPractice_raycast_script/javaPractice.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title JavaPractice
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon images/java.png
+# @raycast.packageName JavaPractice
+
+# Documentation:
+# @raycast.description Script to open CodeBloom in Visual Studio Code.
+# @raycast.author Alfardil Alam
+# @raycast.authorURL https://github.com/alfardil
+
+PROJECT_PATH="/Users/alfardilalam/javaPractice"
+
+VSCODE_APP="/Applications/Visual Studio Code.app"
+
+open -a "$VSCODE_APP" "$PROJECT_PATH"
+
+echo "Opened JavaPractice!"
